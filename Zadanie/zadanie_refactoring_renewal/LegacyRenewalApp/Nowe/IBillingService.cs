d24708cb0faaa8029552ee8dfffb3290ec52009f
s34314 <s34314@pjwstk.edu.pl>
2026-04-07 18:28:50 +0200

    Zrobione Interface

---
+++ Zadanie/zadanie_refactoring_renewal/LegacyRenewalApp/Nowe/IBillingService.cs
@@ -0,0 +1,11 @@
+﻿namespace LegacyRenewalApp;
+
+public interface IBillingService {
+    void Save(RenewalInvoice invoice);
+}
+
+public class BillingService : IBillingService {
+    public void Save(RenewalInvoice invoice) {
+        LegacyBillingGateway.SaveInvoice(invoice);
+    }
+}
---
+++ Zadanie/zadanie_refactoring_renewal/LegacyRenewalApp/Nowe/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace LegacyRenewalApp;
+
+public interface ICustomerRepository
+{
+    Customer GetById(int id);
+}
+
+public interface ISubscriptionPlanRepository {
+    SubscriptionPlan GetByCode(string code);
+}
